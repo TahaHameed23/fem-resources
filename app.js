7759d433829126c3ceab8e933d49f281415bbc8b
TahaHameed23 <89855847+TahaHameed23@users.noreply.github.com>
2023-11-19 19:07:29 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+import http from 'node:http';
+import fs from 'node:fs';
+
+
+const hostname = '127.0.0.1';
+const port = 3004;
+
+const data = fs.readFileSync('./resources.json');
+const resources = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json'); // Set the content type to JSON
+    res.end(JSON.stringify({ resources })); // Serve a JSON response
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
