22ae97e493ff9bfcd6776c13e2a29b6890717a41
TahaHameed23 <89855847+TahaHameed23@users.noreply.github.com>
2023-11-19 21:18:28 +0530

    fix

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-import http from 'node:http';
-import fs from 'node:fs';
+import express from 'express';
+import fs from 'fs';
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-
-const hostname = process.env.HOSTNAME || '127.0.0.1';
 const port =process.env.PORT || 3004;
 
-const data = fs.readFileSync('./resources.json');
-const resources = JSON.parse(data);
+const resources = JSON.parse(fs.readFileSync('./resources.json'));
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json'); // Set the content type to JSON
-    res.end(JSON.stringify({ resources })); // Serve a JSON response
+
+
+app.get('/', (req, res) => {
+    res.end(JSON.stringify({ resources })); 
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});     
